1c5d6fa0f803653e82a3559059f0607d0d736c8e
Z-T-H <112755085+ZTH7@users.noreply.github.com>
2023-10-05 11:39:04 +0200

    read&write string

--- Practica2/ejercicio2/read_strings.c
+++ Practica2/ejercicio2/read_strings.c
@@ -17,11 +17,40 @@
 char *loadstr(FILE *file)
 {
 	/* To be completed */
-	return NULL;	
+	int length = 0, num = 0;
+	char *c = malloc(1);
+	
+	while ((num = fread(c, 1, 1, file)) == 1) {
+		length++;
+	}
+
+	fseek(file,0,SEEK_SET);
+
+	char *res = malloc(length + 1);
+
+	fread(res, 1, length, file);
+
+	return res;
 }
 
 int main(int argc, char *argv[])
 {
 	/* To be completed */
+	FILE *file;
+	char *str, *token;
+
+	if (argc!=2) {
+		fprintf(stderr,"Usage: %s <file_name>\n",argv[0]);
+		exit(1);
+	}
+	if ((file = fopen(argv[1], "r")) == NULL)
+		err(2,"The input file %s could not be opened",argv[1]);
+
+	str = loadstr(file);
+
+	while((token = strsep(&str, "."))!=NULL) {
+		printf("%s\n", token);
+	}
+
 	return 0;
 }
--- Practica2/ejercicio2/write_strings.c
+++ Practica2/ejercicio2/write_strings.c
@@ -6,5 +6,26 @@
 int main(int argc, char* argv[])
 {
 	/* To be completed */
+	FILE *file;
+	char *espacio = ".";
+	if (argc > 1) {
+		if ((file=fopen(argv[1],"wx"))==NULL) {
+				fprintf(stderr, "The output file %s could not be opened: ",
+				        argv[1]);
+				perror(NULL);
+				exit(EXIT_FAILURE);
+			}
+	}
+	else {
+		fprintf(stderr, "Parameters missing");
+				perror(NULL);
+				exit(EXIT_FAILURE);
+	}
+	for(int i = 2; i < argc; i++) {
+		fwrite(argv[i], 1, strlen(argv[i]), file);
+		fwrite(espacio, 1, 1, file);
+	}
+
+	fclose(file);
 	return 0;
 }
