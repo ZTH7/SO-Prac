0f8bf134d7263c722259e4f80b82546ea694333e
itslubin <lubinyee@gmail.com>
2023-12-06 20:33:01 +0100

    f6

---
+++ ficheros_p6/cocinero.c
@@ -0,0 +1,99 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <stdbool.h>
+
+#define M 10
+#define SHARED_MEM_NAME "/caldero_shared_mem"
+#define COOK_SEM_NAME "/cook_semaphore"
+#define SAV_SEM_NAME "/savage_semaphore"
+#define MTX_NAME "/mutex"
+
+struct data {
+    int servings;
+    int cook_waiting; /* # de hilos bloqueados en cook_queue (0 o 1) */
+    int nr_sav_waiting; /* # de salvajes bloqueados */  
+};
+
+volatile int finish = 0;
+
+void handler(int signo) {
+    finish = 1;
+}
+
+void putServingsInPot(int m, sem_t *cook_queue, sem_t *sav_queue, sem_t *sem_mtx, struct data *shared_mem)
+{
+    sem_wait(sem_mtx); /*lock*/
+
+    while (shared_mem->servings > 0) {
+        /*cond wait*/
+        shared_mem->cook_waiting++;
+        sem_post(sem_mtx);
+        sem_wait(cook_queue);
+        sem_wait(sem_mtx);
+    }
+
+    printf("Cocinero: cooking %d meals\n", m);
+    shared_mem->servings = m;
+
+    /*cond broadcast*/
+    while (shared_mem->nr_sav_waiting > 0) {
+        sem_post(sav_queue);
+        shared_mem->nr_sav_waiting--;
+    }
+
+    sem_post(sem_mtx); /*unlock*/
+}
+
+void cook(sem_t *cook_queue, sem_t *sav_queue, sem_t *sem_mtx, struct data *shared_mem)
+{
+    while (!finish) {
+        putServingsInPot(M, cook_queue, sav_queue, sem_mtx, shared_mem);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    signal(SIGINT, handler);
+	signal(SIGTERM, handler);
+
+    // Shared memory setup
+    int shm_fd = shm_open(SHARED_MEM_NAME, O_CREAT | O_RDWR, 0666);
+    ftruncate(shm_fd, sizeof(struct data));
+    struct data *shared_mem = mmap(0, sizeof(struct data), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+    shared_mem->servings = 0;
+    shared_mem->cook_waiting = 0;
+    shared_mem->nr_sav_waiting = 0;
+
+    // Semaphore setup
+    sem_t *cook_queue = sem_open(COOK_SEM_NAME, O_CREAT, 0666, 0); /* Initialize with 0 */
+    sem_t *sav_queue = sem_open(SAV_SEM_NAME, O_CREAT, 0666, 0); /* Initialize with 0 */
+    sem_t *sem_mtx = sem_open(MTX_NAME, O_CREAT, 0666, 1); /* Initialize with 1 */
+
+    if (cook_queue == SEM_FAILED || sav_queue == SEM_FAILED || sem_mtx == SEM_FAILED) {
+        perror("sem_open");
+        exit(EXIT_FAILURE);
+    }
+
+    // Main loop
+    cook(cook_queue, sav_queue, sem_mtx, shared_mem);
+
+    // Cleanup
+    munmap(shared_mem, sizeof(struct data));
+    close(shm_fd);
+    sem_close(cook_queue);
+    sem_close(sav_queue);
+    sem_close(sem_mtx);
+    shm_unlink(MTX_NAME);
+    sem_unlink(COOK_SEM_NAME);
+    sem_unlink(SAV_SEM_NAME);
+    shm_unlink(SHARED_MEM_NAME);
+
+    return 0;
+}
---
+++ ficheros_p6/salvajes.c
@@ -0,0 +1,91 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <stdbool.h>
+
+
+#define NUMITER 3
+#define SHARED_MEM_NAME "/caldero_shared_mem"
+#define COOK_SEM_NAME "/cook_semaphore"
+#define SAV_SEM_NAME "/savage_semaphore"
+#define MTX_NAME "/mutex"
+
+struct data {
+    int servings;
+    int cook_waiting; /* # de hilos bloqueados en cook_queue (0 o 1) */
+    int nr_sav_waiting; /* # de salvajes bloqueados */  
+};
+
+void eat(void)
+{
+    unsigned long id = (unsigned long) getpid();
+    printf("Savage %lu eating\n", id);
+}
+
+bool getServingsFromPot(sem_t *cook_queue, sem_t *sav_queue, sem_t *sem_mtx, struct data *shared_mem)
+{
+    sem_wait(sem_mtx); /*lock*/
+    
+    while (shared_mem->servings == 0) {
+        /*signal*/
+        if (shared_mem->cook_waiting > 0) {
+            sem_post(cook_queue);
+            (shared_mem->cook_waiting)--;
+        }
+         /*cond wait*/
+        (shared_mem->nr_sav_waiting)++;
+        sem_post(sem_mtx);
+        sem_wait(sav_queue);
+        sem_wait(sem_mtx);
+    }
+
+    (shared_mem->servings)--;
+    printf("%d", shared_mem->servings);
+
+    sem_post(sem_mtx); /*unlock*/
+
+    return true;
+}
+
+void savages(sem_t *cook_queue, sem_t *sav_queue, sem_t *sem_mtx, struct data *shared_mem)
+{
+    for (int i = 0; i < NUMITER; i++) {
+        if (getServingsFromPot(cook_queue, sav_queue, sem_mtx, shared_mem)) {
+            eat();
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    // Shared memory setup
+    int shm_fd = shm_open(SHARED_MEM_NAME, O_RDWR, 0666);
+    struct data *shared_mem = mmap(0, sizeof(struct data), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+    // Semaphore setup
+    sem_t *cook_queue = sem_open(COOK_SEM_NAME, O_RDWR);
+    sem_t *sav_queue = sem_open(SAV_SEM_NAME, O_RDWR);
+    sem_t *sem_mtx = sem_open(MTX_NAME, O_RDWR);
+
+    // Check if semaphore creation is successful
+    if (cook_queue == SEM_FAILED || sav_queue == SEM_FAILED || sem_mtx == SEM_FAILED) {
+        perror("sem_open");
+        exit(EXIT_FAILURE);
+    }
+
+    // Main loop
+    savages(cook_queue, sav_queue, sem_mtx, shared_mem);
+
+    // Cleanup
+    munmap(shared_mem, sizeof(struct data));
+    close(shm_fd);
+    sem_close(cook_queue);
+    sem_close(sav_queue);
+    sem_close(sem_mtx);
+
+    return 0;
+}
